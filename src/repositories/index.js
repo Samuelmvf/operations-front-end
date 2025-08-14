6f54cf4abfd516629b3fb8866aff20fc540934f5
Samuel Maia <samuelmvf@gmail.com>
2025-08-14 04:47:55 -0300

    feat: Create index.js for centralized repository exports

---
+++ src/repositories/index.js
@@ -0,0 +1,7 @@
+// Base repository
+export { BaseRepository } from "./base-repository.js";
+
+export { AuthRepository, authRepository } from "./auth-repository.js";
+export { OperationsRepository, operationsRepository } from "./operations-repository.js";
+export { RecordsRepository, recordsRepository } from "./records-repository.js";
+export { UsersRepository, usersRepository } from "./users-repository.js";
